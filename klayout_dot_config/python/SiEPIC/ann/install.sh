dbca3566d5fdb5a2a952f40db69305d201765381
sequoiap <sequoiap4@gmail.com>
2018-12-13 14:58:28 -0700

    Python package installation script for GUI functionality update

--- klayout_dot_config/python/SiEPIC/ann/install.sh
+++ klayout_dot_config/python/SiEPIC/ann/install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # If we decide to use graphviz to create schematics...
-#apt-get install graphviz
-python3 -m pip install -r requirements.txt
+sudo apt-get install graphviz libgraphviz-dev pkg-config
+sudo python3 -m pip install -r requirements.txt
--- klayout_dot_config/python/SiEPIC/ann/tk_circuit_simulation.py
+++ klayout_dot_config/python/SiEPIC/ann/tk_circuit_simulation.py
@@ -1,6 +1,4 @@
 
-# Enter your Python code here
-
 import tkinter as tk
 
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
@@ -29,18 +27,31 @@ class CircuitSimulationGUI():
         self.parent = parent
         # Hide the parent until the whole window has loaded.
         self.parent.withdraw()
+        # Set the window title and size
         self.parent.title('Circuit Simulation')
-        self.parent.geometry('1200x900')
+        w = 1600 # width for the Tk root
+        h = 900 # height for the Tk root
+        # get screen width and height
+        ws = self.parent.winfo_screenwidth() # width of the screen
+        hs = self.parent.winfo_screenheight() # height of the screen
+        # calculate x and y coordinates for the Tk root window
+        x = (ws - w) / 2
+        y = (hs - h) / 2
+        # set the dimensions of the screen and where it is placed
+        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        # Initialize the menu and figures
         self.create_menu()
         self.init_figures()
-        self.update_magnitude()
-        self.generate_phase()
         self.generate_schematic()
+        self.update_magnitude()
+        self.update_phase()
+        # Now that everything is in place, show the window.
         self.parent.after(0, self.parent.deiconify)
         
     def init_figures(self):
         # Schematic figure initialization
-        self.schematic = tk.Frame(self.parent, height=850, width=400, relief="ridge", bd=5)
+        self.schematic = tk.Frame(self.parent, height=870, width=895, relief="ridge", bd=5)
+        self.schematic.grid(row=0, column=0, rowspan=2)
         
         # Magnitude plot initialization
         self.magnitude_plot = tk.Frame(self.parent, width=700, height=400)
@@ -50,7 +61,7 @@ class CircuitSimulationGUI():
         self.mag_canvas.get_tk_widget().pack(side=tk.TOP)#, fill=tk.X)#, expand=1)
         self.mag_toolbar = NavigationToolbar2Tk(self.mag_canvas, self.magnitude_plot)
         self.mag_canvas.get_tk_widget().pack(side=tk.TOP)#, fill=tk.BOTH)#, expand=1)
-        self.magnitude_plot.grid(column=1)#, fill=tk.BOTH)#, expand=1)
+        self.magnitude_plot.grid(row=0, column=1)#, fill=tk.BOTH)#, expand=1)
         
         # Phase plot initialization
         self.phase_plot = tk.Frame(self.parent, width=700, height=400)
@@ -60,50 +71,43 @@ class CircuitSimulationGUI():
         self.phase_canvas.get_tk_widget().pack(side=tk.TOP)#, fill=tk.X)#, expand=1)
         self.phase_toolbar = NavigationToolbar2Tk(self.phase_canvas, self.phase_plot)
         self.phase_canvas.get_tk_widget().pack(side=tk.TOP)
-        self.phase_plot.grid(column=1)
+        self.phase_plot.grid(row=1, column=1)
         
     def update_magnitude(self):
-        ##frame = self.magnitude_plot
-        ##fig = Figure(figsize=(7, 4), dpi=100)
+        # Get s parameters and frequencies
         s, f = gs.getSparams()
+        # Convert from Hz to THz
         tera = 1e12
         f = np.divide(f, tera)
-        ##ax = self.mag_fig.add_subplot(111)]
+        # Clear whatever is on the plot, overlay new graph
         self.mag_ax.clear()
         self.mag_ax.plot(f, abs(s[:,0,2])**2)
+        # Label the plot
         self.mag_ax.set_xlabel('Frequency (THz)')
         self.mag_ax.set_ylabel(r'$|A| ^2$')
         self.mag_ax.set_title('Magnitude-Squared')
-        ##canvas = FigureCanvasTkAgg(fig, master=frame)
+        # Draw on the canvas, update the toolbar
         self.mag_canvas.draw()
-        ##canvas.get_tk_widget().pack(side=tk.TOP)#, fill=tk.X)#, expand=1)
-        ##toolbar = NavigationToolbar2Tk(canvas, frame)
         self.mag_toolbar.update()
-        ##canvas.get_tk_widget().pack(side=tk.TOP)#, fill=tk.BOTH)#, expand=1)
-        ##frame.grid(column=1)
     
-    def generate_phase(self):
-        ##frame = self.phase_plot
-        ##fig = Figure(figsize=(7, 4), dpi=100)
+    def update_phase(self):
+        # Get s parameters and frequencies
         s, f = gs.getSparams()
+        # Convert from Hz to THz
         tera = 1e12
         f = np.divide(f, tera)
-        ##ax = fig.add_subplot(111)
+        # Clear whatever is on the plot, overlay new graph
         self.phase_ax.clear()
+        # Label the plot
         self.phase_ax.plot(f, np.rad2deg(np.unwrap(np.angle(s[:,0,2]))))
         self.phase_ax.set_xlabel('Frequency (THz)')
         self.phase_ax.set_title('Phase')
-        ##canvas = FigureCanvasTkAgg(fig, master=frame)
+        # Draw on the canvas, update the toolbar
         self.phase_canvas.draw()
-        ##canvas.get_tk_widget().pack(side=tk.TOP)#, fill=tk.X)#, expand=1)
-        ##toolbar = NavigationToolbar2Tk(canvas, frame)
         self.phase_toolbar.update()
-        ##canvas.get_tk_widget().pack(side=tk.TOP)#, fill=tk.BOTH)#, expand=1)
-        ##frame.grid(column=1)
         
     def generate_schematic(self):
-        frame = self.schematic
-        frame.grid(row=0, column=0, rowspan=2)
+        return
     
     def _quit(self):
         self.parent.withdraw()
@@ -119,7 +123,7 @@ class CircuitSimulationGUI():
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Open")
         filemenu.add_command(label="Save")
-        filemenu.add_command(label="Exit", command=self._quit)
+        filemenu.add_command(label="Exit", command=self.parent.on_closing)
         menubar.add_cascade(label="File", menu=filemenu)
         
         # Configure the menubar
