744167738312edb2a7c1936ebe59e62c4034648f
Hyrum Gunther <hygunth@gmail.com>
2019-01-14 09:21:46 -0800

    added grating coupler functionality

--- klayout_dot_config/python/SiEPIC/ann/qopticParser1.py
+++ klayout_dot_config/python/SiEPIC/ann/qopticParser1.py
@@ -3,6 +3,7 @@ import io
 #import skrf as rf
 import scipy.io as sio
 import numpy as np
+import cmath as cm
 import json as js
 from pprint import pprint
 
@@ -26,33 +27,52 @@ def r2p(x):
 
 
 class Reader:
-    def readSparamData(filename, numports):
-        fid = open(filename, "r")
-        line = fid.readline()
-        line = fid.readline()
-        numrows = int(tuple(line[1:-2].split(','))[0])
+    #include bool for grating coupler
+    def readSparamData(filename, numports, isgc):
         F = []
-        S = np.zeros((numrows, numports, numports), dtype='complex128')
-        r = m = n = 0
-        for line in fid:
-            if(line[0] == '('):
-                continue
-            data = line.split()
-            data = list(map(float, data))
-            if(m == 0 and n == 0):
-                F.append(data[0])
-            S[r,m,n] = p2r(data[1], data[2])
-            r += 1
-            if(r == numrows):
-                r = 0
-                m += 1
-                if(m == numports):
-                    m = 0
-                    n += 1
-                    if(n == numports):
-                        break
+        S = []
+        fid = open(filename, "r")
+        if isgc is True:
+            arrlen = 67-33
+            lines = fid.readlines()
+            F = np.zeros(arrlen)
+            S = np.zeros((arrlen,2,2), 'complex128')
+            for i in range(0, arrlen):
+                words = lines[i+33].split()
+                F[i] = float(words[0])
+                S[i,0,0] = cm.rect(float(words[1]), float(words[2]))
+                S[i,0,1] = cm.rect(float(words[3]), float(words[4]))
+                S[i,1,0] = cm.rect(float(words[5]), float(words[6]))
+                S[i,1,1] = cm.rect(float(words[7]), float(words[8]))
+            F = F[::-1]
+            S = S[::-1,:,:]
+        
+        else:
+            line = fid.readline()
+            line = fid.readline()
+            numrows = int(tuple(line[1:-2].split(','))[0])
+            S = np.zeros((numrows, numports, numports), dtype='complex128')
+            r = m = n = 0
+            for line in fid:
+                if(line[0] == '('):
+                    continue
+                data = line.split()
+                data = list(map(float, data))
+                if(m == 0 and n == 0):
+                    F.append(data[0])
+                S[r,m,n] = p2r(data[1], data[2])
+                r += 1
+                if(r == numrows):
+                    r = 0
+                    m += 1
+                    if(m == numports):
+                        m = 0
+                        n += 1
+                        if(n == numports):
+                            break
         fid.close()
         return [S, F]
+        
 pya.Cell.Reader = Reader
 
 def findPort(name, list):
