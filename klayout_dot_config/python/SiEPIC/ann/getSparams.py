aba5e66eafc1d836770774349dd003b189091718
Sequoia Ploeg <sequoiap4@gmail.com>
2019-05-08 15:43:40 -0600

    Removed uncalled functions

--- klayout_dot_config/python/SiEPIC/ann/getSparams.py
+++ klayout_dot_config/python/SiEPIC/ann/getSparams.py
@@ -49,33 +49,3 @@ def getPorts():
     os.chdir(orig_cwd)
     # Return the sparameters and the frequency array
     return p
-    
-def plot_magnitude():
-    import matplotlib.pyplot as plt
-    s, f = getSparams()
-    plt.plot(f, abs(s[:,0,2])**2)
-    plt.xlabel('Frequency (THz)')
-    plt.title('Magnitude^2')
-    plt.show()
-    #print(len(s[:,0,2]))
-    #print(len(f))
-    
-def plot_phase():
-    import matplotlib.pyplot as plt
-    import numpy as np
-    s, f = getSparams()
-    #plt.plot(f, np.rad2deg(np.unwrap(np.angle(s[:,0,2]))))
-    plt.plot(f, np.unwrap(np.angle(s[:,0,2])))
-    plt.xlabel('Frequency (THz)')
-    plt.title('Phase (deg)')
-    plt.show()
-
-# def create_matlab_files():
-#     import scipy.io as sio
-#     # First, change the current working directory because we'll be saving files
-#     os.chdir(temp_cwd)
-#     sio.savemat(matname, mdict={'sparams':s})
-#     sio.savemat(freqname, mdict={'freq':f})
-#     # Change the working directory back to what it was originally, 
-#     # out of politeness and an abundance of caution
-#     os.chdir(orig_cwd)
