79354c2685b9a3cb46ab1383dce40554f292dbda
Sequoia Ploeg <sequoiap4@gmail.com>
2019-05-21 19:08:42 -0600

    Condensed related circuit simulation GUI code

--- klayout_dot_config/python/SiEPIC/ann/circuit_simulation.py
+++ klayout_dot_config/python/SiEPIC/ann/circuit_simulation.py
@@ -2,20 +2,44 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-# from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
-# from matplotlib.backend_bases import key_press_handler
-# from matplotlib.figure import Figure
-
-from SiEPIC.ann import getSparams as gs
 from SiEPIC.ann import NetlistDiagram
-from SiEPIC.ann.layout.graphing.graph import Graph, DataSet, MenuItem
+from SiEPIC.ann.graph import Graph, DataSet, MenuItem
 import SiEPIC._globals as glob
-
 from SiEPIC.ann.simulation import Simulation
 
 import scipy.io as sio
 import numpy as np
 import os
+
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+
+class SchematicDrawer:
+    def __init__(self, parent : tk.Toplevel, components):
+        self.components = components
+        # The master tk object
+        self.parent = parent
+        self.master = tk.Toplevel(parent)
+        # self.master = parent
+        self.master.title("Layout")
+
+        # The only real objects we'll need to interact with to plot and unplot
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+
+        # Objects needed simply for the sake of embedding the graph in tk
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.master)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.fig.clear()
+        self.ax = self.fig.add_subplot(111)
+
+    def draw(self):
+        for comp in self.components:
+            self.ax.plot(comp.posx, comp.posy, 'ro')
+            externals = [x for x in comp.ports if x < 0 ]
+            self.ax.text(comp.posx, comp.posy, "  Port " + str(-externals[0]) + ": " + comp.label)
+        self.ax.axis('off')
+        self.canvas.draw()
 
 class TkRoot(tk.Tk):
     def __init__(self):
@@ -214,9 +238,8 @@ class CircuitAnalysisGUI():
         self.phase = None
 
     def open_layout(self):
-        import SiEPIC.ann.layout.schematic as schem
         comp = self.simulation.external_components
-        fig = schem.SchematicDrawer(self.parent, comp)
+        fig = SchematicDrawer(self.parent, comp)
         fig.draw()
 
     def frequencyToWavelength(self, frequencies):
