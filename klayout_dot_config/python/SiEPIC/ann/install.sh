24c8eeff4a5a28fcf2852c8192cb462439fe05b7
Sequoia Ploeg <sequoiap4@gmail.com>
2019-05-22 11:45:35 -0600

    Reduced required install packages

--- klayout_dot_config/python/SiEPIC/ann/install.sh
+++ klayout_dot_config/python/SiEPIC/ann/install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 # If we decide to use graphviz to create schematics...
-echo "SiEPIC ANN requires graphviz and tkinter packages."
-sudo apt-get install graphviz libgraphviz-dev pkg-config
-sudo apt-get install python3-tk python3-pil python3-pil.imagetk
+echo "SiEPIC ANN requires tkinter packages."
+# sudo apt-get install python3-tk
 python3 -m pip install --user -r requirements.txt
