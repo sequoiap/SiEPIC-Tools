a2e1ac755b79ff3b7a2b168eae695ca6d3e2131c
Sequoia Ploeg <sequoiap4@gmail.com>
2019-05-21 19:07:36 -0600

    Export clean-up

--- klayout_dot_config/python/SiEPIC/ann/__init__.py
+++ klayout_dot_config/python/SiEPIC/ann/__init__.py
@@ -1,5 +1,5 @@
 # Alec Hammond's ANN Python integration
 
-from . import qopticParser1, loadNN, export_netlist, waveguideNN, getSparams, NetlistDiagram, circuit_simulation, layout, simulation, model, monte_carlo_simulation
+from . import loadNN, netlist, waveguideNN, getSparams, NetlistDiagram, circuit_simulation, simulation, model, monte_carlo_simulation
 
 print('ANN Python integration (CamachoLab)')
