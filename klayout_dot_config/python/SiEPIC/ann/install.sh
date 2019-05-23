6b9767c4c7ebb2e25cd766b4ff666b8abeea0bba
Sequoia Ploeg <sequoiap4@gmail.com>
2019-05-23 00:09:11 -0600

    Created serializable building blocks for netlist

--- klayout_dot_config/python/SiEPIC/ann/install.sh
+++ klayout_dot_config/python/SiEPIC/ann/install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # If we decide to use graphviz to create schematics...
-echo "SiEPIC ANN requires tkinter packages."
+# echo "SiEPIC ANN requires tkinter packages."
 # sudo apt-get install python3-tk
 python3 -m pip install --user -r requirements.txt
--- klayout_dot_config/python/SiEPIC/ann/models/components.py
+++ klayout_dot_config/python/SiEPIC/ann/models/components.py
@@ -16,7 +16,7 @@ INSTALLED_COMPONENTS = [
 #######################################
 
 from importlib import import_module
-import json
+import jsons
 from enum import Enum
 
 LOADED_MODELS = {}
@@ -28,47 +28,119 @@ for component in INSTALLED_COMPONENTS:
 # for key, val in LOADED_MODELS.items():
 #     print(key, val)
 
+class Component:
+    """This class represents an arbitrary component in the netlist. All attributes can
+    be initialized as keyword arguments in the __init__ function.
+
+    This is the base class for all netlist components. All components have a type and a
+    list of ports.
+
+    Attributes
+    ----------
+    compType : str
+        The name of the component type.
+    ports : list of ints
+        A list of all port connections (required to be integers).
+    """
+    compType: str
+    ports: list
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a Component dataclass.
+
+        Parameters
+        ----------
+        compType : str
+            The name of the component type.
+        ports : list of ints
+            A list of all port connections (required to be integers).
+        """
+        if 'compType' in kwargs:
+            self.compType = kwargs.get('compType')
+        if 'ports' in kwargs:
+            self.ports = kwargs.get('ports')
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+class Waveguide(Component):
+    """This class represents a waveguide component in the netlist. All attributes can
+    be initialized as keyword arguments in the __init__ function.
+
+    This class inherits from Component and inherits all of its data members.
+
+    Attributes
+    ----------
+    compType : str
+        The name of the component type.
+    ports : list of ints
+        A list of all port connections (required to be integers).
+    length : float
+        Total waveguide length.
+    width : float
+        Designed waveguide width.
+    height : float
+        Designed waveguide height.
+    """
+    length: float
+    width: float
+    height: float
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a Waveguide dataclass, which inherits from Component.
+
+        Parameters
+        ----------
+        compType : str
+            The name of the component type.
+        ports : list of ints
+            A list of all port connections (required to be integers).
+        length : float
+            Total waveguide length.
+        width : float
+            Designed waveguide width.
+        height : float
+            Designed waveguide height.
+        """
+        super().__init__(*args, **kwargs)
+        if 'length' in kwargs:
+            self.length = kwargs.get('length')
+        if 'width' in kwargs:
+            self.width = kwargs.get('width')
+        if 'height' in kwargs:
+            self.height = kwargs.get('height')
+
 class CompType(Enum):
     '''
     Enum listing of all available component types.
+
+    Name, value pairs comprise of the component type as shown on the netlist, paired with
+    its dataclass model as the value.
     '''
 
-    BDC = 'ebeam_bdc_te1550'
-    DC = 'ebeam_dc_halfring_te1550'
-    GC = 'ebeam_gc_te1550'
-    YB = 'ebeam_y_1550'
-    TR = 'ebeam_terminator_te1550'
-    WG = 'ebeam_wg_integral_1550'
+    ebeam_bdc_te1550 = Component
+    ebeam_dc_halfring_te1550 = Component
+    ebeam_gc_te1550 = Component
+    ebeam_y_1550 = Component
+    ebeam_terminator_te1550 = Component
+    ebeam_wg_integral_1550 = Waveguide
 
-class Component(dict):
-    def __init__(self, compType=NotImplemented):
-        self.compType = compType
-        self.ports = []
+def component_factory(JSONobj: str):
+    """Creates one of the objects that inherits from Component (based on the value of compType)
+    given the object's JSON representation.
 
-class Waveguide(Component):
-    def __init__(self):
-        self.length = 0
-        self.width = 0
-        self.height = 0
+    Parameters
+    ----------
+    JSONobj : str
+        The string representation of the JSON object.
+    """
+    instance = jsons.load(JSONobj, Component)
+    
+    for type_ in CompType:
+        if instance.compType == type_.name:
+            return jsons.load(JSONobj, type_.value)
 
-def parse_component(obj, verbose=False):
-    if verbose:
-        print(obj['type'])
-
-    if obj['type'] == "Waveguide":
-        component = Waveguide()
-    else:
-        component = Component()
-    component.__dict__.update(obj)
-    return component
-
-s = '[{ "type" : "Waveguide", "length" : 50, "width" : 500, "height" : 220 }, { "type" : "ybranch", "ports" : 50 }]'
-o = json.loads(s, object_hook=parse_component)
-
-o1 = Waveguide()
-o1.compType = CompType.WG
-o2 = Component()
-o2.compType = CompType.BDC
-
-objs = [o1, o2]
-print(json.dumps(objs))
+if __name__ == "__main__":
+    c = Waveguide(compType='ebeam_wg_integral_1550', ports=[1,2], length=50, width=60, height=70)
+    dumped = jsons.dump(c)
+    obj = component_factory(dumped)
