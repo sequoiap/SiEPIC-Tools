95a6e2d90f40109941946937c246cb7440fe7eb3
Sequoia Ploeg <sequoiap4@gmail.com>
2019-05-22 19:25:35 -0600

    Added two waveguide model libraries

---
+++ klayout_dot_config/python/SiEPIC/ann/models/wg1550_lumerical/model.py
@@ -0,0 +1,13 @@
+
+
+class Model:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_s_params(frequency, length, width, thickness, dLength):
+        pass
+
+    @staticmethod
+    def about():
+        print("Lumerical model: uses a third order polynomial to approximate the phase through a waveguide.")
---
+++ klayout_dot_config/python/SiEPIC/ann/models/wg_ann/model.py
@@ -0,0 +1,13 @@
+
+
+class Model:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_s_params(width, thickness, dLength):
+        pass
+
+    @staticmethod
+    def about():
+        print("ANN model: uses a polynomial fit trained by a neural network to approximate the phase through a waveguide.")
