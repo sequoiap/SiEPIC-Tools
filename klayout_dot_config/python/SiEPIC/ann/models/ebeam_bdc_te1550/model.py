0013054f2d2bc4799b4d5fbc4fa3b5c61f7c1df3
Sequoia Ploeg <sequoiap4@gmail.com>
2019-05-23 21:03:54 -0600

    New bdc component

---
+++ klayout_dot_config/python/SiEPIC/ann/models/ebeam_bdc_te1550/model.py
@@ -0,0 +1,15 @@
+
+
+class Model:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_s_params(*args, **kwargs):
+        pass
+
+    @staticmethod
+    def about():
+        message = "About ebeam_bdc_te1550:"
+        print(message)
+
