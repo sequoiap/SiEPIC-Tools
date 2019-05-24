257c858454cccca567345145f01f489fc3255425
Sequoia Ploeg <sequoiap4@gmail.com>
2019-05-24 17:24:53 -0600

    Added new attributes to models

--- klayout_dot_config/python/SiEPIC/ann/models/components.py
+++ klayout_dot_config/python/SiEPIC/ann/models/components.py
@@ -27,6 +27,10 @@ INSTALLED_COMPONENTS = [
     'wg_ann',
     'wg1550_lumerical',
     'ebeam_bdc_te1550',
+    'ebeam_y_1550',
+    'ebeam_dc_halfring_te1550',
+    'ebeam_terminator_te1550',
+    'ebeam_gc_te1550',
 ]
 
 """
@@ -48,15 +52,17 @@ except:
     for component in INSTALLED_COMPONENTS:
         mod = import_module(component)
         LOADED_MODELS[component] = mod.Model
+
+from abc import ABC, abstractmethod
+from SiEPIC.ann.simulation import SimulationSetup as simset
 """
 END DO NOT ALTER
 """
-
-class Component:
+class Component(ABC):
     """This class represents an arbitrary component in the netlist. All attributes can
     be initialized as keyword arguments in the __init__ function.
 
-    This is the base class for all netlist components. All components have a type, a
+    This is the abstract base class for all netlist components. All components have a type, a
     list of nets, x/y layout positions, and a list of simulation models from which the
     component in question could retrieve s-parameters.
 
@@ -69,25 +75,41 @@ class Component:
         A dictionary of installed models (packages) from which this component could 
         retrieve s-parameters. This is a class attribute, and is not stored at the instance 
         level or in JSON output. It's format is {'[Human Readable Name]': '[Model Location]'}.
-    # _selected_model : str
-    #     A key from the _simulation_models dictionary.
+    _selected_model : str
+        A key from the _simulation_models dictionary.
+    _model_ref : class
+        A reference to the Model class of the currently selected model for this component type.
+        Provides a hook for calling functions like 'get_s_params'.
 
     Attributes
     ----------
     component_type : str
         The name of the component type.
     nets : list of ints
-        A list of all port connections (required to be integers).
+        A list of all connected nets (required to be integers) following the same order that 
+        the device's port numbering scheme follows.
     lay_x : float
         The x-position of the component in the overall layout.
     lay_y : float
         The y-position of the component in the overall layout.
+    port_count : int
+        The number of ports on the device, calculated by counting the number of connected nets.
+        This attribute is a property and is not settable.
+
+    Methods
+    -------
+    set_model(key: str)
+        Class method for selecting a simulation model for the specified component.
+    get_s_params(*args, **kwargs) : np.array, np.array
+        Abstract method that each class implements. Each classes passes in the necessary parameters
+        to its model and returns the frequency and s-parameter matrices.
     """
     component_type: str = None
     nets: list = []
     lay_x: float = 0
     lay_y: float = 0
     _simulation_models: dict = {}
+    _selected_model: str
 
     @staticmethod
     def setup(obj):
@@ -119,6 +141,15 @@ class Component:
         cls._selected_model = key
         cls._model_ref = LOADED_MODELS[cls._simulation_models[cls._selected_model]] if cls._selected_model else None
 
+    @property
+    def port_count(self):
+        return len(self.nets)
+
+    @abstractmethod
+    def get_s_params(self):
+        freq, sparams = self._model_ref.get_s_params(self.port_count)
+        return simset.interpolate(freq, sparams)
+
     def __str__(self):
         return 'Object::' + str(self.__dict__)
 
@@ -136,19 +167,19 @@ class ebeam_wg_integral_1550(Component):
     length : float
         Total waveguide length.
     width : float
-        Designed waveguide width.
+        Designed waveguide width in microns (um).
     height : float
-        Designed waveguide height.
+        Designed waveguide height in microns (um).
     """
     length: float = 0
-    width: float = 500e-9
-    height: float = 220e-9
+    width: float = 0.5
+    height: float = 0.22
     radius: float = 0
     points: list = []
 
     _simulation_models = {
-        'ANN': 'wg_ann',
-        'Lumerical': 'wg1550_lumerical',
+        # 'ANN Waveguide': 'wg_ann',
+        'EBeam Waveguide': 'wg1550_lumerical',
     }
 
     def __init__(self, *args, **kwargs):
@@ -173,6 +204,9 @@ class ebeam_wg_integral_1550(Component):
             self.height = kwargs.get('height')
         if 'points' in kwargs:
             self.points = kwargs.get('points')
+
+    def get_s_params(self, delta_length=0):
+        return self._model_ref.get_s_params(simset.FREQUENCY_RANGE, self.length, self.width, self.height, delta_length)
 
 
 
@@ -200,6 +234,9 @@ class ebeam_bdc_te1550(Component):
         """
         super().__init__(*args, **kwargs)
 
+    def get_s_params(self):
+        return super().get_s_params()
+
 
 
 
@@ -214,7 +251,7 @@ class ebeam_gc_te1550(Component):
     Inherited
     """
     _simulation_models = {
-        
+        'EBeam Grating Coupler': 'ebeam_gc_te1550',
     }
     
     def __init__(self, *args, **kwargs):
@@ -225,6 +262,9 @@ class ebeam_gc_te1550(Component):
         Inherited
         """
         super().__init__(*args, **kwargs)
+
+    def get_s_params(self):
+        return super().get_s_params()
 
 
 
@@ -240,7 +280,7 @@ class ebeam_y_1550(Component):
     Inherited
     """
     _simulation_models = {
-        
+        'EBeam Y-Branch': 'ebeam_y_1550',
     }
     
     def __init__(self, *args, **kwargs):
@@ -251,6 +291,9 @@ class ebeam_y_1550(Component):
         Inherited
         """
         super().__init__(*args, **kwargs)
+    
+    def get_s_params(self):
+        return super().get_s_params()
 
 
 
@@ -266,7 +309,7 @@ class ebeam_terminator_te1550(Component):
     Inherited
     """
     _simulation_models = {
-        
+        'EBeam Nanotaper Terminator': 'ebeam_terminator_te1550',
     }
     
     def __init__(self, *args, **kwargs):
@@ -277,6 +320,9 @@ class ebeam_terminator_te1550(Component):
         Inherited
         """
         super().__init__(*args, **kwargs)
+
+    def get_s_params(self):
+        return super().get_s_params()
 
 
 
@@ -292,7 +338,7 @@ class ebeam_dc_halfring_te1550(Component):
     Inherited
     """
     _simulation_models = {
-        
+        'EBeam DC Halfring': 'ebeam_dc_halfring_te1550',
     }
     
     def __init__(self, *args, **kwargs):
@@ -304,6 +350,9 @@ class ebeam_dc_halfring_te1550(Component):
         """
         super().__init__(*args, **kwargs)
 
+    def get_s_params(self):
+        return super().get_s_params()
+
 
 
 
@@ -313,207 +362,6 @@ IMPLEMENTATION CODE BELOW, PROCEED WITH CAUTION!
 
 """
 
-def strToSci(str):
-    '''
-    local function to convert strings written in Klayout's 
-    exponential notation into floats
-    Args:
-        str (str): string to convert to float
-    Returns:
-        float representation of the input string
-    '''
-
-    ex = str[-1]
-    base = float(str[:-1])
-    if(ex == 'm'):
-        return base * 1e-3
-    elif(ex == 'u'):
-        return base * 1e-6
-    elif(ex == 'n'):
-        return base * 1e-9
-    else:
-        return float(str(base) + ex)
-
-class Parser:
-    '''
-    The Parser class reads a netlist generated by the SiEPIC toolbox and uses 
-    various classes which inherit from 'models.components.Component' to create an 
-    object based model of a photonic circuit. 
-    
-    Each derived class is connected to a component model in 'models' that exposes a 
-    'get_s_params' method with its appropriate arguments to the derived model. These 
-    s_params are the s-matrices of the component, which are then used to simulate the 
-    circuit's transmission behavior.
-
-    Attributes
-    ----------
-    component_list : list
-        A list of objects derived from 'models.components.Component' representing the
-        photonic circuit.
-    nports : int
-        A counter keeping track of the total number of nets in the circuit (0-indexed).
-
-    Methods
-    -------
-    parse_file(filepath)
-        Parses through the netlist to identify components and organize them into objects.
-        Objects are connected with their data models, allowing them to retrieve any
-        available parameters.
-    _parse_line(line_elements)
-        Reads the elements on a line of the netlist (already delimited before passed to
-        _parse_line) and creates the appropriate object. Appends the newly created object
-        to the Parser's component_list.
-
-    # 'cascadeCells' takes the cellList gathered by 'parseFile' and cascades all the s-matrices together
-    # using scikit-rf's 'connect_s' and 'innerconnect_s' functions, deleting already connected Cells as
-    # it goes. The result is a single Cell object with an s-matrix representing the cascaded circuit
-    '''
-
-    def __init__(self):
-        '''
-        Initializes a Parser and creates a structure to hold a list of components
-        and count the number of nets in the circuit (0-indexed).
-        '''
-        self.component_list = []
-        self.nports = 0
-
-
-    def parse_file(self, filepath: str):
-        '''
-        reads the netlist file and calls 'parseCell' to create Cell objects from 
-        the netlist entries
-        Args:
-            none
-            self.filepath is the needed path to the netlist
-        Returns
-            none
-            the call to 'parseCell' will add a new Cell to self.cellList
-        '''
-
-        with open(filepath) as fid:
-            text = fid.read()
-            return self.parse_text(text)
-        #     lines = fid.readlines()
-        #     for line in lines:
-        #         elements = line.split()
-        #         if len(elements) > 0:
-        #             if (".ends" in elements[0]):
-        #                 break
-        #             elif ("." in elements[0]) or ("*" in elements[0]):
-        #                 continue
-        #             else:
-        #                 self._parse_line(elements)
-        # return self.component_list
-
-    def parse_text(self, text: str):
-        lines = text.splitlines()
-        for line in lines:
-                elements = line.split()
-                if len(elements) > 0:
-                    if (".ends" in elements[0]):
-                        break
-                    elif ("." in elements[0]) or ("*" in elements[0]):
-                        continue
-                    else:
-                        self._parse_line(elements)
-        return self.component_list
-        
-
-    def _parse_line(self, line_elements: list):
-        '''
-        Parses a line from the netlist, already split into individual elements, and 
-        converts it into a new Component object.
-        
-        Parameters
-        ----------
-        line_elements : list
-            A list of all the elements on a line (already split by some delimiter).
-        '''
-
-        component = None
-        nets = []
-        for item in line_elements[1:]:
-            if "N$" in item:
-                net = str(item).replace("N$", '')
-                nets.append(net)
-                if int(net) > self.nports:
-                    self.nports = int(net)
-                continue
-            elif component is None:
-                component = create_component_by_name(item)
-            elif "lay_x=" in item:
-                component.lay_x = float(str(item).replace("lay_x=", ''))
-            elif "lay_y=" in item:
-                component.lay_y = float(str(item).replace("lay_y=", ''))
-            elif "radius=" in item:
-                component.radius = float(str(item).replace("radius=", ''))
-            elif "wg_length=" in item:
-                lenth = str(item).replace("wg_length=", '')
-                component.length = strToSci(lenth)
-            elif "wg_width=" in item:
-                width = str(item).replace("wg_width=", '')
-                component.width = strToSci(width)
-            elif "points=" in item:
-                # The regex, in case you ever need it: /(\[[\d]+[\,][\d]+\])/g
-                points = str(item).replace("points=", '')
-                points = points.replace("\"[[", '')
-                points = points.replace("]]\"", '')
-                point_list = points.split('],[')
-                for point in point_list:
-                    out = point.split(',')
-                    component.points.append((float(out[0]), float(out[1])))
-        component.nets = nets
-        self.component_list.append(component)
-
-
-    # def cascadeCells(self):
-    #     '''
-    #     For each pin in the circuit, the s-matrices of the Cells containing that pin are cascaded
-    #     using scikit-rf funtions. 'innerconnect_s' if the two occurances of the pin are in the
-    #     same Cell, 'connect_s' if they are in two different cells
-
-    #     For a pin:
-    #     * 'findPortMatch' is called to find where the two occurances of the pin are
-    #     * If they are in the same Cell, use 'innerconnect_s' to cascade the s-matrices and delete the
-    #         connected ports from the Cell's port list
-    #     * If they are in different Cells, create a new Cell object and let its s-matrix be the result 
-    #         of 'connect_s' for the two Cells. Delete the two Cells from the cellList
-
-    #     Repeat this process until all pins have been connected
-
-    #     One Cell will remain in the cellList. Its s-matrix represents the transmission behavior of the
-    #     circuit as a whole
-    #     '''
-
-    #     if self.nports == 0:
-    #         return
-    #     for n in range(0, self.nports + 1):
-    #         ca, ia, cb, ib = findPortMatch(str(n), self.cellList)
-
-    #         #if pin occurances are in the same Cell
-    #         if ca == cb:
-    #             self.cellList[ca].s = rf.innerconnect_s(self.cellList[ca].s, ia, ib)
-    #             del self.cellList[ca].p[ia]
-    #             if ia < ib:
-    #                 del self.cellList[ca].p[ib-1]
-    #             else:
-    #                 del self.cellList[ca].p[ib]
-
-    #         #if pin occurances are in different Cells
-    #         else:
-    #             d = Cell()
-    #             d.f = self.cellList[0].f
-    #             d.s = rf.connect_s(self.cellList[ca].s, ia, self.cellList[cb].s, ib)
-    #             del self.cellList[ca].p[ia]
-    #             del self.cellList[cb].p[ib]
-    #             d.p = self.cellList[ca].p + self.cellList[cb].p
-    #             del self.cellList[ca]
-    #             if ca < cb:
-    #                 del self.cellList[cb-1]
-    #             else:
-    #                 del self.cellList[cb]
-    #             self.cellList.append(d)
-
 # Finish setting all class variables for component subclasses
 comp_subclasses = [class_ for class_ in Component.__subclasses__()]
 for class_ in comp_subclasses:
@@ -522,30 +370,22 @@ for class_ in comp_subclasses:
 def create_component_by_name(component_name: str):
     return getattr(sys.modules[__name__], component_name)()
 
-def netlist_lumerical_to_json(filename):
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    print(os.getcwd())
-    components = Parser().parse_file(filename)
-    output = jsons.dump(components, verbose=True, strip_privates=True)
-    with open('netlist.json', 'w') as outfile:
-        json.dump(output, outfile, indent=2)
-
 
 import os
 if __name__ == "__main__":
-    w1 = ebeam_wg_integral_1550(length=50e-6, width=500.05129e-9, height=220)
-    w2 = ebeam_wg_integral_1550(nets=[2,3], length=150e-6, width=499.5129e-9, height=220)
-    items = []
-    items.append(w1)
-    items.append(w2)
-    output = jsons.dump(items, verbose=True, strip_privates=True)
-    with open('data.json', 'w') as outfile:
-        json.dump(output, outfile, indent=2)
-    data = None
-    with open('data.json') as jsonfile:
-        data = json.load(jsonfile)
-    inputstr = jsons.load(data)
-    # os.remove('data.json')
-    LOADED_MODELS[ebeam_wg_integral_1550._simulation_models['Lumerical']].about()
-
-    netlist_lumerical_to_json('netlist.txt')
+    # w1 = ebeam_wg_integral_1550(length=50e-6, width=500.05129e-9, height=220)
+    # w2 = ebeam_wg_integral_1550(nets=[2,3], length=150e-6, width=499.5129e-9, height=220)
+    # items = []
+    # items.append(w1)
+    # items.append(w2)
+    # output = jsons.dump(items, verbose=True, strip_privates=True)
+    # with open('data.json', 'w') as outfile:
+    #     json.dump(output, outfile, indent=2)
+    # data = None
+    # with open('data.json') as jsonfile:
+    #     data = json.load(jsonfile)
+    # inputstr = jsons.load(data)
+    # # os.remove('data.json')
+    # LOADED_MODELS[ebeam_wg_integral_1550._simulation_models['EBeam Waveguide']].about()
+    bdc = ebeam_bdc_te1550(nets=[0, 1], lay_x=0, lay_y=0)
+    f, s = bdc.get_s_params()
