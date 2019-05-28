ba89e6a206546c8020ab28cf7546495e095f2860
Sequoia Ploeg <sequoiap4@gmail.com>
2019-05-28 11:05:16 -0600

    Trying to integrate new wg model

--- klayout_dot_config/python/SiEPIC/ann/models/wg_ann/model.py
+++ klayout_dot_config/python/SiEPIC/ann/models/wg_ann/model.py
@@ -2,8 +2,48 @@ import os
 import numpy as np
 from SiEPIC.ann.models.wg_ann import waveguideNN as wn
 
+import pickle
+
 path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "NN_SiO2_neff.h5")
 model = wn.loadWaveguideNN(path)
+
+def cartesian_product(arrays):
+    la = len(arrays)
+    dtype = np.find_common_type([a.dtype for a in arrays], [])
+    arr = np.empty([len(a) for a in arrays] + [la], dtype=dtype)
+    for i, a in enumerate(np.ix_(*arrays)):
+        arr[..., i] = a
+    return arr.reshape(-1, la)
+
+def straightWaveguide(wavelength, width, thickness, angle):
+    #load regression
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'R_straight_update.pkl'), 'rb') as handle:
+            LR_straight = pickle.Unpickler(handle).load()
+
+    # Santize the input
+    if type(wavelength) is np.ndarray:
+        wavelength = np.squeeze(wavelength)
+    else:
+        wavelength = np.array([wavelength])
+    if type(width) is np.ndarray:
+        width = np.squeeze(width)
+    else:
+        width = np.array([width])
+    if type(thickness) is np.ndarray:
+        thickness = np.squeeze(thickness)
+    else:
+        thickness = np.array([thickness])
+    if type(angle) is np.ndarray:
+        angle = np.squeeze(angle)
+    else:
+        angle = np.array([angle])
+
+    # Run through regression
+    INPUT  = cartesian_product([wavelength,width,thickness,angle])
+
+    OUTPUT = LR_straight.predict(INPUT)
+    
+    return OUTPUT
 
 class Model:
     def __init__(self):
@@ -34,6 +74,7 @@ class Model:
 
         # effective index is calculated by the ANN
         neff = wn.getWaveguideIndex(model,np.transpose(wl),width,thickness,mode)
+        # n2 = straightWaveguide(1.550, width, thickness, 90)
 
         #K is calculated from the effective index and wavelength
         K = (2*np.pi*np.true_divide(neff,wl))
