8e516f56149699349e7bda8aee3690e7cebcccc2
Sequoia Ploeg <sequoiap4@gmail.com>
2019-05-28 11:05:07 -0600

    Added ability for settings to persist between sessions

--- klayout_dot_config/python/SiEPIC/ann/__init__.py
+++ klayout_dot_config/python/SiEPIC/ann/__init__.py
@@ -17,4 +17,38 @@ __author__ = 'Sequoia Ploeg, Hyrum Gunther'
 
 from . import *
 
-print('ANN Python integration (CamachoLab)')
+import atexit
+import configparser
+from importlib import import_module
+import os
+
+def on_open():
+    print('ANN Python Integration (CamachoLab)')
+    try:
+        config = configparser.ConfigParser()
+        config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), "settings.ini"))
+        selections = config['MODEL_SELECT']
+
+        mod = import_module('.models', 'SiEPIC.ann')
+        Comp = mod.components.Component
+        for class_ in Comp.__subclasses__():
+            class_.set_model(selections[class_.__name__])
+    except:
+        print("Persistent settings could not be read.")
+
+def on_close():
+    config = configparser.ConfigParser()
+    config['MODEL_SELECT'] = {}
+    selections = config['MODEL_SELECT']
+
+    mod = import_module('.models', 'SiEPIC.ann')
+    Comp = mod.components.Component
+    for class_ in Comp.__subclasses__():
+        selections[class_.__name__] = class_._selected_model
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "settings.ini"), 'w') as configfile:
+        config.write(configfile)
+    print("ANN Integration Closed")
+
+on_open()
+
+atexit.register(on_close)
